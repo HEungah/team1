3e8dcc651b4875c82a3c5735051af431735a7bd4
504 <504@DESKTOP-G6EJM93>
2023-06-27 15:52:21 +0900

    230627_규리(1커밋)

--- team_1/public/teamproject/틱택토.js
+++ team_1/public/teamproject/틱택토.js
@@ -1,3 +1,25 @@
-/**
- * 
- */
+
+
+//규리 컴퓨터 랜덤 함수
+
+console.log('연결됨');
+
+
+function 컴퓨터난수생성(){ //컴퓨터난수생성 함수 s
+	// console.log(Math.random());
+	// console.log(Math.random()+1);
+	// console.log(Math.random()*9+1);
+	console.log(ParseInt(Math.random()*9+1)); // 1부터 9사이의 난수생성
+
+
+	//if (사람이 클릭한 곳과 같지 않으면 함수실행){
+		//같으면 다시 위로 가서 다시 난수 뽑아서 실행 
+	//}
+	
+} // 컴퓨터난수생성 함수 e
+
+console.log(컴퓨터난수생성);
+
+
+
+
