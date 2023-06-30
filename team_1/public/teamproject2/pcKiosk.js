1c595f8ff8e2fd826fae60bcc77a2e19cca90c9c
504 <504@DESKTOP-G6EJM93>
2023-06-30 16:30:42 +0900

    04:30_ 김규리

--- team_1/public/teamproject2/pcKiosk.js
+++ team_1/public/teamproject2/pcKiosk.js
@@ -7,7 +7,7 @@ let userList = []; // 회원정보리스트
 
 let seatList = []; // 좌석
 
-let payList = [];  // 금액
+let payList = [ 1000 , 3000 , 5000, 10000 ];  // 금액
 
 let seatposition = false;    //좌석 선택
 
@@ -46,7 +46,13 @@ function selectSeat() { // 좌석선택 함수
 
 //규리
 function selectTm() { // 시간금액선택 함수 
-   //인자 인덱스
+  //인자 인덱스
+   // 1. 1,2,3,4 금액 버튼 누르면
+   // 2. paylist 배열에 저장되고
+   // 3. 누른 버튼의 금액이 하단 화면에 출력됨
+   console.log('버튼실행'); // 확인용
+   // document.querySelector('.timeClick').
+   
 }   //  시간금액선택 함수 e 
 
 
