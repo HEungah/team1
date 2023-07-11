8e6633caad6c35a95a752d66ff781343d8671d24
keembloo <guesstty@naver.com>
2023-07-11 19:12:53 +0900

    0711_김규리 19:12

--- teamProject1/src/main/webapp/js/menuinfo.js
+++ teamProject1/src/main/webapp/js/menuinfo.js
@@ -12,11 +12,18 @@ let addoption = [ { name : '한솥밥 밥 양 적게' , price : 0 , category: 'r
 				 
 let menuimginfo = [ 'newmenu5_info1.jpg' , 'newmenu5_info2.jpg'];
 
+let checkList = [];
 
 let option = document.querySelector('.option');
 	
 	
 let imgcount = 0;
+
+let riceCount =[0, 0 , 0];
+
+let sum = 4500;
+
+let totalPrice = 0;
 
 
  //이미지교체
@@ -27,8 +34,6 @@ setInterval(() => {
 		imgcount = 0;}
 	img.src = `../img/img_kim/${menuimginfo[imgcount]}`;
 }, 4000);
-
-
 
 
 optionPrint(0); // 최초한번실행
@@ -53,20 +58,28 @@ function optionPrint(select) {
 } // optionPrint end
 
 
-let riceCount =[0, 0 , 0];
+
+
 // 추가금액 출력함수 하나 만들어보쟈 ㅠ
 function plusPrint (select){
 	// console.log('플러스함수됨')
 	let html = ``;
 	let plusPrice = document.querySelector('.plus_price');
 	let checkbox = document.getElementsByName("rice");
-	// console.log(checkbox[select]);
+	  console.log(checkbox[select].checked);
 	
 	if ( addoption[select].category == 'rice' ) {
+	/*	if (checkbox[select].checked == true) {
+			checkbox[select].checked = false;
+			console.log('if');
+		} else if (checkbox[select].checked == false){
+			console.log('elseif');*/
+		checkbox[select].checked = true; // 선택한것만 다시 체크된상태로 만듬
+		//}// if 2 end 
+		
 		for(let i=0; i< riceCount.length; i++){ // 밥클릭하면 일단 다 체크해제상태로 만듬
 			checkbox[i].checked = false;
 		}
-		checkbox[select].checked =true; // 선택한것만 다시 체크된상태로 만듬
 	} // if 1 end
 	
 	
@@ -85,29 +98,31 @@ function plusPrint (select){
 
 
 
-let sum = 4500;
-let totalPrice = 0;
-let ricePrice = 0;
-// 총합계구하는 함수
+
+
+
+// 총합계구해서 출력하는 함수
 function total( select ){
 	// console.log('토탈됨')
 		let sumPrice = document.querySelector('.sum_price');
+		let checkbox = document.querySelectorAll('input[type="checkbox"]');
 		
-		for(let i=0; i<addoption.length; i++){
-			if( i == select ){
-				ricePrice = (addoption[i].price+sum)
-				sumPrice.innerHTML = ricePrice.toLocaleString();
-				return;
-			} // if end
-		} // for end
-		
-		for (let j = 0; j < addoption.length; j++) {
-    	let option = addoption[j];
-    	if (option.category == 'toping' && option.checked) {
-    	  totalPrice += option.price+ricePrice;
-    }
-  }
-    return totalPrice;
+		//체크리스트배열에 체크된 인덱스넣기
+		checkList=[]; // 클릭할때마다 한번 초기화
+		totalPrice =sum;
+		for (let i = 0; i < checkbox.length; i++) {
+ 		 if (checkbox[i].checked) {
+   			checkList.push(i);
+   			// 체크된 인덱스랑비교해서 가격을 토탈에 넣기
+   			totalPrice += addoption[i].price;
+   			
+ 		 } //if end
+		} //for 1 end
+		//console.log(checkList);
+   
+    sumPrice.innerHTML = totalPrice.toLocaleString();
+
+  
 } // function end
 
 
