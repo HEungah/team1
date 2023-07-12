1517016a0d01e871a9dffb30dc4be0df71a34012
504 <504@DESKTOP-2TRGUA3>
2023-07-12 15:37:55 +0900

    무시해주세요

--- teamProject1/src/main/webapp/js/view_go.js
+++ teamProject1/src/main/webapp/js/view_go.js
@@ -1,22 +1,17 @@
 console.log('js실행')
 
-//클릭 시 누른 글 no 가져옴
+//클릭 시 누른 글 no
 let no =JSON.parse( localStorage.getItem('no'));
-
 //쿠키에 있는 글 리스트 가져옴
 let qList= JSON.parse(localStorage.getItem('qList'));
 			if(qList==null){qList=[]};
-
+//페이지 내에서 앞/뒤 페이지를 가져올 인덱스를 찾기 위해 list.js에서 만듦
 let indexN=JSON.parse(localStorage.getItem('indexN'))
 
-console.log(no)
+onView()
 
-onView()//
-
-// 클릭한 글 출력 함수---------------------------------
-
-function onView(){ console.log('onView 함수 실행')
-	
+// 상세페이지 출력 함수---------------------------------
+function onView(){
 	// 어디에 받을건지
 	let title= document.querySelector('.v_title')
 	let content=document.querySelector('.v_content')
@@ -25,7 +20,7 @@ function onView(){ console.log('onView 함수 실행')
 	
 	for(let i=0; i<qList.length;i++){
 		let q= qList[i]
-		
+		// 전체 게시물의 no와 상세페이지 no가 동일할 때 함수 실행
 		if(q.no==no){
 			title.innerHTML=q.title;
 			content.innerHTML=q.content; 
@@ -38,23 +33,20 @@ function onView(){ console.log('onView 함수 실행')
 	
 }
 
-//
+// 전체 게시물 중 클릭한 페이지를 가져오기 위해 인수로 실행
+function onViewLoad( no ){
 
-function onViewLoad( no ){ // 인수 : 클릭한 제목(게시물)의 번호 
-	console.log( ' 현재 클릭된 제목(게시물)의 번호 : ' + no);
-	// 클릭된 결과를 다른페이지로 옮기는방법
-		// JS는 페이지가 전환/새로고침 초기화 -> // 세션/쿠키
-	// 1. 클릭된 게시물번호 세션에 저장 
+	// 클릭된 결과를 다른페이지로 옮기기
 	localStorage.setItem( 'no' , no )
-		// * 조회수 증가 함수 호출 
-		//increaseView( no )
-	// 2. 페이지 이동 
+		// 조회수 증가 함수 
+		increaseView( no )
+	// 페이지 이동 
 	location.href="view_go.jsp"
 }
 
 
 
-// 3. 조회수 증가함수=수정 [ 실행조건 : 제목 클릭후 페이지 전환 전에 ]
+//  조회수 증가함수
 function increaseView( no ){
 	// 1. 클릭된 게시물번호의 게시물찾기
 	for( let i = 0 ; i<qList.length ; i++ ){
@@ -70,36 +62,25 @@ function increaseView( no ){
 
 
 //뒤로가기 버튼 onclick
-function back(){ console.log('뒤로가기 성공')
-
-	location.href='list_go.jsp' 
-	
-}
+function back(){ location.href='list_go.jsp' }
 
 // 왼쪽 넘기기 onclick
 function left(){
-	console.log(no)
-	
 	if(0 == indexN){return;}
 	indexN--
-	
+	//전역변수 사용
 	localStorage.setItem( 'indexN' , JSON.stringify( indexN))
-	
+	// no 숫자로 받기. 이전글을 가기 위해 -1
 	onViewLoad(Number(no)-1) 
-	
-	
-}
+	}
 
 // 오른쪽 넘기기 onclick
 function right(){
-
+	//리스트의 마지막 인덱스와 동일할 때 실
 	if(qList.length-1==indexN){return;}
 	indexN++
-	
 	localStorage.setItem( 'indexN' , JSON.stringify( indexN))
-	 
-		
-	
+	// 다음 글로 가기 위해 +1씩 증가 
 	onViewLoad(Number(no)+1)
 	
 }
